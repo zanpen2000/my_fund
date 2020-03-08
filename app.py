afbe2f9f12a14370cd70575c89b0be6cc73be12d
peng <zanpen2000@gmail.com>
2020-03-08 20:12:01 +0800

    基金估值更新，并通知到微信

---
+++ app.py
@@ -0,0 +1,71 @@
+import datetime
+import json
+import openpyxl
+
+import requests
+import time
+import logging
+
+logger = logging.getLogger("app")
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
+
+excel_file = '/home/peng/文档/fund/Fund.xlsx'
+
+sckey = 'SCU88470T2b933a481a8f217a209c8482c05e0f535e64b2a99dff4'
+
+
+def send_msg(title, text):
+    msg_push_url = F"https://sc.ftqq.com/{sckey}.send?text={title}&desp={text}"
+    resp = requests.get(msg_push_url)
+    logger.debug(resp)
+    logger.info("已将消息推送到微信")
+
+
+def get_timestamp():
+    millis = int(round(time.time() * 1000))
+    return millis
+
+
+def get_url(url, params=None, proxies=None):
+    rsp = requests.get(url, params=params, proxies=proxies)
+    rsp.raise_for_status()
+    return rsp.text
+
+
+def get_fund_valuation(code):
+    ts = get_timestamp()
+    url = F"http://fundgz.1234567.com.cn/js/{code}.js?rt={ts}"
+    now = datetime.datetime.now()
+    json_data = json.loads(get_url(url)[8:-2])
+    logger.info(F"{now}: {code} - {json_data['name']} -> {json_data['dwjz']}")
+    return json_data
+
+
+def update_fund_value():
+    wb = openpyxl.load_workbook(excel_file)
+    sheet = wb['基金']
+    green_fund_list = ["|名称|成本单价|估值|跌幅|", "| :---- | :-------- | ----: | ---: |"]
+    for r in sheet.rows:
+        fund_code = str(r[0].value)
+        if fund_code[0] in '0123456789':
+            data = get_fund_valuation(fund_code)  # get val and write back
+            from openpyxl.styles import numbers, colors, Font
+            r[4].number_format, r[7].number_format, r[8].number_format = numbers.FORMAT_GENERAL
+            r[9].number_format = numbers.FORMAT_PERCENTAGE_00
+            r[7].value, r[8].value, r[9].value = float(data['dwjz']), float(data['gsz']), float(data['gszzl']) / 100
+            if r[4].value > r[8].value:
+                p = str(round(float((((r[8].value - r[4].value) / r[4].value) * 100)), 2)) + '%'
+                line = F"|{r[1].value} | {r[8].value} | {r[4].value}| {p} "
+                green_fund_list.append(line)
+                r[8].font = Font(color=colors.RED)
+
+    wb.save(excel_file)
+    wb.close()
+    send_msg('以下基金估算净值低于成本单价', '\r\n'.join(green_fund_list))
+
+
+if __name__ == '__main__':
+    update_fund_value()
+    logger.info("\nFund valuation updated.")
